d9073fa5a1b275561dc8749866666ced523f635f
rcflorestal <rcflorestal@yahoo.com.br>
2020-11-16 13:59:18 -0300

    The Data Scientist Toolbox

---
+++ The Data Scientist Toolbox/R_script.R
@@ -0,0 +1,18 @@
+print('This script was createde within RStudio')
+print('and nows it lives in GitHub')
+
+# Reference: Book R Programming for Data Science by Roger D. Peng
+# This book is for sale at http://leanpub.com/rprogramming
+
+# Entering Input
+# The '<-' symbol is the assignment operator
+x <- 1
+print(x)
+
+msg <- "Hello"
+
+# Evaluation
+x <- 11:30 ## ':' creates a sequence of integers from 11 to 30.
+
+print(x) ## Explicit printing
+x  ## Auto-printing
