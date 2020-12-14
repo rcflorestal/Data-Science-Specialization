3ce104a38b9a5bfbb48b90e34490fe5bef89aa13
Robson Cruz <rcflorestal@yahoo.com.br>
2020-12-14 20:42:14 -0300

    Summarizing data frame

--- Getting-and-Cleaning-Data/summarizing-data/summarizing_data.R
+++ Getting-and-Cleaning-Data/summarizing-data/summarizing_data.R
@@ -9,7 +9,7 @@
 ## Summarizing Data.
 
 ## Set work directory.
-setwd("C:/Data-Science-Foundations-using-R-Specialization/Getting-and-Cleaning-Data/summarizing-data/")
+setwd("C:/Data-Science-Foundations-using-R-Specialization/Getting-and-Cleaning-Data/summarizing-data")
 
 ## Download the data frame.
 ## Set link.
@@ -91,3 +91,86 @@ fakeData = rnorm(1e5)
 object.size(fakeData)
 
 print(object.size(fakeData), units = "Mb")
+
+## Creating Sequences
+s1 <- seq(1, 10, by = 2); s1
+
+s2 <- seq(1, 10, length = 3); s2
+
+x <- c(1, 3, 8, 25, 100); seq(along = x)
+
+## Subsetting variables
+df$nearMe = df$neighborhood %in% c("Roland Park", "Homeland")
+table(df$nearMe)
+
+## Creating Binary Variables
+df$zipWrong = ifelse(df$zipCode < 0, TRUE, FALSE)
+table(df$zipWrong, df$zipCode < 0)
+
+## Easier Cutting
+library(Hmisc)
+df$zipGroups = cut2(df$zipCode, g = 4)
+table(df$zipGroups)
+
+## Creating Factor Variables
+df$zcf <- factor(df$zipCode)
+df$zcf[1:10]
+class(df$zcf)
+
+
+## Level of Factor Variables
+yesno <- sample(c("yes", "no"), size = 10, replace = TRUE)
+
+yesnofac = factor(yesno, levels = c("yes", "no"))
+
+relevel(yesnofac, ref = "yes")
+
+as.numeric(yesnofac)
+
+## Reshape Data
+library(reshape2)
+
+head(mtcars)
+
+## Melting Data Frames
+mtcars$carName <- rownames(mtcars)
+carMelt <- melt(mtcars, 
+                id = c("carName", "gear", "cyl"), 
+                measure.vars = c("mpg", "hp"))
+
+head(carMelt)
+tail(carMelt)
+
+## Casting Data Frames
+cylData <- dcast(carMelt, cyl ~ variable)
+cylData
+
+cylData <- dcast(carMelt, cyl ~ variable, mean)
+cylData
+
+## Averaging Values
+head(InsectSprays)
+
+tapply(InsectSprays$count, InsectSprays$spray, sum)
+
+## Another way - split
+spIns = split(InsectSprays$count, InsectSprays$spray)
+spIns
+
+## Another Way = apply
+sprCount = lapply(spIns, sum)
+sprCount
+
+## Another way - combine
+unlist(sprCount)
+
+sapply(spIns, sum)
+
+## Another way - plyr package
+ddply(InsectSprays, .(spray), summarize, sum = sum(count))
+
+## Creating a new variable
+spraySums <- ddply(InsectSprays, .(spray), summarize, sum = ave(count, FUN = sum))
+dim(spraySums)
+head(spraySums)
+tail(spraySums)
